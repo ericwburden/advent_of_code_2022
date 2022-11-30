2429e4005e0b947c56ac1090bdc1c3bdd7cfdf45
Eric Burden <eric.w.burden@gmail.com>
2022-11-30 07:33:48 -0500

    Add Output support for all integer types

--- src/lib.rs
+++ src/lib.rs
@@ -36,12 +36,43 @@ pub enum Part {
 
 #[derive(Debug, Eq)]
 pub enum Output {
+    U8(u8),
+    U16(u16),
     U32(u32),
-    I32(i32),
     U64(u64),
+    U128(u128),
+    I8(i8),
+    I16(i16),
+    I32(i32),
     I64(i64),
+    I128(i128),
     String(String),
 }
+
+impl From<u8> for Output {
+    fn from(value: u8) -> Self {
+        Output::U8(value)
+    }
+}
+
+impl From<i8> for Output {
+    fn from(value: i8) -> Self {
+        Output::I8(value)
+    }
+}
+
+impl From<u16> for Output {
+    fn from(value: u16) -> Self {
+        Output::U16(value)
+    }
+}
+
+impl From<i16> for Output {
+    fn from(value: i16) -> Self {
+        Output::I16(value)
+    }
+}
+
 
 impl From<u32> for Output {
     fn from(value: u32) -> Self {
@@ -67,6 +98,18 @@ impl From<i64> for Output {
     }
 }
 
+impl From<u128> for Output {
+    fn from(value: u128) -> Self {
+        Output::U128(value)
+    }
+}
+
+impl From<i128> for Output {
+    fn from(value: i128) -> Self {
+        Output::I128(value)
+    }
+}
+
 impl From<String> for Output {
     fn from(value: String) -> Self {
         Output::String(value)
@@ -76,10 +119,16 @@ impl From<String> for Output {
 impl Display for Output {
     fn fmt(&self, f: &mut Formatter<'_>) -> DisplayResult {
         match self {
+            Output::U8(v) => write!(f, "{v}"),
+            Output::U16(v) => write!(f, "{v}"),
             Output::U32(v) => write!(f, "{v}"),
-            Output::I32(v) => write!(f, "{v}"),
             Output::U64(v) => write!(f, "{v}"),
+            Output::U128(v) => write!(f, "{v}"),
+            Output::I8(v) => write!(f, "{v}"),
+            Output::I16(v) => write!(f, "{v}"),
+            Output::I32(v) => write!(f, "{v}"),
             Output::I64(v) => write!(f, "{v}"),
+            Output::I128(v) => write!(f, "{v}"),
             Output::String(v) => write!(f, "{v}"),
         }
     }
