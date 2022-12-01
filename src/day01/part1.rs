9b445b090b92168c73f2c4794e14306cbfe8d62c
Eric Burden <eric.w.burden@gmail.com>
2022-12-01 06:36:30 -0500

    Solve Day 1

--- src/day01/part1.rs
+++ src/day01/part1.rs
@@ -1,5 +1,8 @@
 use crate::day01::{Input, Output};
 
+/// Solve Day 01, Part 01
 pub fn solve(input: &Input) -> Output {
-    unimplemented!()
+    // Get the maximum calorie count for an Elf
+    // and return it as an Output::U32.
+    input.iter().copied().max().unwrap().into()
 }
