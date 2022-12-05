529139ac322bab52e065616b02993ae7d958380b
Eric Burden <eric.w.burden@gmail.com>
2022-12-05 12:33:40 -0500

    Remove commented code

--- src/day05/part2.rs
+++ src/day05/part2.rs
@@ -24,16 +24,4 @@ impl Execute for CrateStacks {
             self[(*destination - 1) as usize].push(ch);
         }
     }
-}
-
-//impl Execute for CrateStacks {
-//    fn execute(&mut self, instruction: &Instruction) {
-//        let Instruction { count, origin, destination } = instruction;
-//        let origin = (*origin - 1) as usize;
-//        let destination = (*destination - 1) as usize;
-//        let split_idx = self[origin].len() - (*count as usize) - 1;
-//        for ch in self[origin].drain(split_idx..) {
-//            self[destination].push(ch);
-//        }
-//    }
-//}
+} 
