b6f9bf66d63f41a845cb1644e6697bbcae2ac2ed
Eric Burden <eric.w.burden@gmail.com>
2022-12-21 00:11:34 -0500

    Solve Day 20

--- src/day20/input.rs
+++ src/day20/input.rs
@@ -3,5 +3,25 @@ use crate::day20::Input;
 const INPUT: &str = include_str!("../../input/20/input.txt");
 
 pub fn read() -> Input {
-    unimplemented!()
+    INPUT
+        .lines()
+        .flat_map(|l| l.parse::<i64>())
+        .collect()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::collections::HashSet;
+
+    #[test]
+    fn check_input() {
+        let input = read();
+        assert_eq!(input.len(), 5000);
+        assert_eq!(input[0], -8023);
+        assert_eq!(input[4999],  5838);
+
+        let input_set = input.iter().collect::<HashSet<_>>();
+        assert_eq!(input.len(), input_set.len());
+    }
 }
