6ea30aa9fd804af62cdc845c726c9cefa41282e6
Eric Burden <eric.w.burden@gmail.com>
2022-12-28 22:32:17 -0500

    Added comments to Day 25 Input Parsing

--- src/day25/input.rs
+++ src/day25/input.rs
@@ -1,8 +1,11 @@
 use crate::day25::Input;
 
+/// This represents one of our SNAFU numbers, which is just a String
+/// in a Wrapper so we can have custom `From` implementations.
 #[derive(Debug, Clone)]
 pub struct Snafu(pub String);
 
+/// Converting a line from the input into a Snafu is super complicated.
 impl From<&str> for Snafu {
     fn from(line: &str) -> Self {
         Snafu(line.to_string())
@@ -11,6 +14,7 @@ impl From<&str> for Snafu {
 
 const INPUT: &str = include_str!("../../input/25/input.txt");
 
+/// Parse that input!
 pub fn read() -> Input {
     INPUT.lines().map(Snafu::from).collect::<Vec<_>>()
 }
