ae01390ad3efc5fb9b4f0a73e3e0e773c470c2ce
Eric Burden <eric.w.burden@gmail.com>
2022-12-28 22:31:45 -0500

    Add comments to Day 25

--- src/day25/part1.rs
+++ src/day25/part1.rs
@@ -1,12 +1,15 @@
 use super::input::Snafu;
 use crate::day25::{Input, Output};
 
+/// Solve Day 25, Part 1
 pub fn solve(input: &Input) -> Output {
     let fuel_cost: i128 = input.iter().cloned().map(i128::from).sum();
     let snafu_cost = Snafu::from(fuel_cost);
     snafu_cost.0.into()
 }
 
+/// Unit conversion from a SNAFU number to a base-10 integer. This is a pretty
+/// common algorithm for converting base-whatever to decimal.
 impl From<Snafu> for i128 {
     fn from(snafu: Snafu) -> Self {
         let mut total = 0;
@@ -25,6 +28,14 @@ impl From<Snafu> for i128 {
     }
 }
 
+/// Unit conversion from a decimal integer into a SNAFU number. This is a much
+/// less common (to me) operation. The tricky bit is handling the fact that we have 
+/// digits whose value is centered around zero instead of anchored at zero on the
+/// least significant place. For a normal base-5 conversion, I'd take the result 
+/// of the remainder % 5, then divide by 5 for each digit until no remainder was
+/// left. Apparently, adding two each round lets us shift the digits. I'm not
+/// 100% sure why this works, but it works. Math, amirite? This solution was inspired
+/// by Google searches about "balanced ternary" number systems.
 impl From<i128> for Snafu {
     fn from(number: i128) -> Self {
         let mut remainder = number;
@@ -39,11 +50,9 @@ impl From<i128> for Snafu {
                 _ => unreachable!(),
             };
             out.push(glyph);
-            remainder += 2;
+            remainder += 2;  // This works for some reason.
             remainder /= 5;
         }
         Snafu(out.chars().rev().collect::<String>())
     }
 }
-// def g(n):
-//     return "" if n == 0 else g((n + 2) // 5) + "012=-"[n % 5]
